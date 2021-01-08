3d70f44fb140c840d66d9a50df1a0e4d9a4e3eec
Tony Barsotti <tony.barsotti@idfive.com>
2021-01-07 21:51:59 -0500

    Adding updated nav that uses patterns from the UMD SPH nav

--- static/src/js/index.ts
+++ static/src/js/index.ts
@@ -8,7 +8,7 @@ import "bootstrap/js/dist/modal";
 import "bootstrap/js/dist/tab";
 import "bootstrap/js/dist/alert";
 import focusWithin from "focus-within";
-import * as objectFitImages from 'object-fit-images';
+import * as objectFitImages from "object-fit-images";
 
 import initSlideshows from "../components/slideshow/slideshow";
 import initCards from "../components/card/card";
@@ -16,12 +16,14 @@ import initCarousels from "../components/carousel/carousel";
 import initStatGroups from "../components/stat-group/stat-group";
 import initSubnav from "../components/subnav/subnav";
 import Tablesaw from "../components/table/table";
+import initSiteHeader from "../components/site-header/site-header";
 
 /**
  * Init
  */
 focusWithin(document);
 objectFitImages();
+initSiteHeader();
 initSlideshows();
 initCards();
 initCarousels();
